19907fe27b822c4118e18f678ce9cfb8108de9a5
ahajessica <jessicaramirez330@gmail.com>
2018-06-06 22:39:20 +0000

    automatically backed up by learn

--- hashes.rb
+++ hashes.rb
@@ -1,39 +1,39 @@
-# #A HASH IS MADE UP OF KEY/VALUE PAIRS
-# #hash = {"cheese" => "the stuff on pizza"}
+# # #A HASH IS MADE UP OF KEY/VALUE PAIRS
+# # #hash = {"cheese" => "the stuff on pizza"}
 
-# character_names = ["Monica","Joey","Phoebe"] #key 
-# characteristics = ["Neat Freak", "Jokester","Free Spirit"] #values 
+# # character_names = ["Monica","Joey","Phoebe"] #key 
+# # characteristics = ["Neat Freak", "Jokester","Free Spirit"] #values 
 
-# friends_characters = {}
+# # friends_characters = {}
 
-# index = 0 
-# character_names.each do |name| 
-#   friends_characters[name] = characteristics[index]
-#   index += 1 #this will iterate through each key 
+# # index = 0 
+# # character_names.each do |name| 
+# #   friends_characters[name] = characteristics[index]
+# #   index += 1 #this will iterate through each key 
+# # end
+
+# # puts friends_characters
+
+# celeb_crush={} #empty hash 
+# celeb_crush["Emi"] = "Grant Gustin"
+
+# # puts celeb_crush #(this prints both key and value pair)
+
+
+# class_celeb_crushes = {
+#   "Natalia" => "Timothee Chalemet",
+#   "Maya" =>  "Michael B. Jordan",
+#   "Amira" => "Cole Sprouse"  
+# }
+
+# # puts class_celeb_crushes
+# # puts class_celeb_crushes.values
+# # puts class_celeb_crushes.keys
+
+# #iterates through the hash to find keys/values 
+# class_celeb_crushes.each do |names, crushes|
+# puts "#{names} crush is #{crushes}" 
 # end
-
-# puts friends_characters
-
-celeb_crush={} #empty hash 
-celeb_crush["Emi"] = "Grant Gustin"
-
-# puts celeb_crush #(this prints both key and value pair)
-
-
-class_celeb_crushes = {
-  "Natalia" => "Timothee Chalemet",
-  "Maya" =>  "Michael B. Jordan",
-  "Amira" => "Cole Sprouse"  
-}
-
-# puts class_celeb_crushes
-# puts class_celeb_crushes.values
-# puts class_celeb_crushes.keys
-
-#iterates through the hash to find keys/values 
-class_celeb_crushes.each do |names, crushes|
- puts "#{names} crush is #{crushes}" 
-end
 
 
 
