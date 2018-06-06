7d527978f27c3bf62588296c664ef9ecc7893664
ahajessica <jessicaramirez330@gmail.com>
2018-06-06 21:52:09 +0000

    automatically backed up by learn

---
+++ hashes.rb
@@ -0,0 +1,32 @@
+# #A HASH IS MADE UP OF KEY/VALUE PAIRS
+# #hash = {"cheese" => "the stuff on pizza"}
+
+# character_names = ["Monica","Joey","Phoebe"] #key 
+# characteristics = ["Neat Freak", "Jokester","Free Spirit"] #values 
+
+# friends_characters = {}
+
+# index = 0 
+# character_names.each do |name| 
+#   friends_characters[name] = characteristics[index]
+#   index += 1 #this will iterate through each key 
+# end
+
+# puts friends_characters
+
+celeb_crush={} #empty hash 
+
+celeb_crush["Emi"] = "Grant Gustin"
+
+puts celeb_crush 
+
+
+
+
+
+
+
+
+
+
+
